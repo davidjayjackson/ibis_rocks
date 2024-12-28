ef11ec05a9b61f72be65ac9ceabfcf89946785ac
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2024-12-28 13:43:36 -0500

    isis/ipython start up.

---
+++ 00-first.py
@@ -0,0 +1,11 @@
+print("import ibis")
+import ibis
+print("ibis.options.interactive = Tru")
+ibis.options.interactive = True
+print("import ibis.selectors as s")
+import ibis.selectors as s
+print("from ibis import _")
+from ibis import _
+print("import seaborn as sns")
+import seaborn as sns
+
